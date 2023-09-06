eda4dbc3fbab847c847ce95149bd6cb0ac42c712
Muhammad Ahsan <mdotihsan@gmail.com>
2023-09-06 21:03:20 +0500

    email form work

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,11 @@
 import Layout from "@/components/layout/Layout"
 import Link from "next/link"
-export default function Contact() {
+import nodemailer from "nodemailer";
 
+export default function Contact() {
+function submitEmail(params) {
+    
+}
     return (
         <>
             <Layout breadcrumbTitle="Contact Us">
@@ -41,7 +45,7 @@ export default function Contact() {
                                         <div className="form-grp">
                                             <textarea id="message" placeholder="Your Message here" />
                                         </div>
-                                        <button className="btn" type="submit">Send Message</button>
+                                        <button onClick={submitEmail()} className="btn" type="submit">Send Message</button>
                                     </form>
                                 </div>
                             </div>
@@ -98,3 +102,85 @@ export default function Contact() {
         </>
     )
 }
+export const sendEmail = async (data) => {
+    let resp ='';
+    const { email, name, phone, subject, message } = data;
+    const transporter = nodemailer.createTransport({
+      host: "mail.sses.pk",
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'info@sses.pk',
+        pass: 'sses_0321',
+      },
+    });
+    const html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>MTFX Connect With</title>
+      <style>
+        table {
+          border-collapse: collapse;
+          width: 100%;
+        }
+        th,
+        td {
+          padding: 8px;
+          text-align: left;
+          border-bottom: 1px solid #ddd;
+        }
+        th {
+          background-color: #f2f2f2;
+        }
+        tfoot td {
+          font-weight: bold;
+        }
+      </style>
+    </head>
+    <body>
+      <table border="1" width="621" style="border:1px solid #222;" cellpadding="0" cellspacing="0">
+      <tbody>
+      <tr>
+      <td style="width: 260px;border:1px solid #222;padding:8px;">Name</td>
+      <td style="width: 345px;border:1px solid #222;padding:8px;">${name}</td>
+      </tr>
+      <tr>
+      <td style="width: 260px;border:1px solid #222;padding:8px;">Email</td>
+      <td style="width: 345px;border:1px solid #222;padding:8px;">${email}</td>
+      </tr>
+      <tr>
+      <td style="width: 260px;border:1px solid #222;padding:8px;">Phone</td>
+      <td style="width: 345px;border:1px solid #222;padding:8px;">${phone}</td>
+      </tr>
+      <td style="width: 260px;border:1px solid #222;padding:8px;">Subject</td>
+      <td style="width: 345px;border:1px solid #222;padding:8px;">${subject}</td>
+      </tr>
+      <tr>
+      <td style="width: 260px;border:1px solid #222;padding:8px;">Message</td>
+      <td style="width: 345px;border:1px solid #222;padding:8px;">${message}</td>
+      </tr>
+      </tbody>
+      </table><br />
+    </body>
+  </html>
+  `;
+    const mailData = {
+        from: 'info@sses.pk',
+      to: 'info.sustainablesolar@gmail.com',
+      subject: 'Message Received (Contact Page)',
+      html: html,
+    };
+    transporter.sendMail(mailData, function (error, info) {
+
+      if (error) {
+        resp = 'Sorry there was an error sending your message. Please try again later'
+        console.error(error);
+      } else {
+        resp = 'Message Sent! Thank you for contacting us.'
+      }
+    });
+    return resp
+  };
