14ab59a04834afa733e140b499c2a0013a52fc7f
Muhammad Ahsan <mdotihsan@gmail.com>
2024-06-06 14:06:53 +0500

    sitemap updated

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,19 +1,43 @@
 function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset
-        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-              http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-  <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
-  
-  
-  <url>
-    <loc>https://www.sses.pk/</loc>
-    <lastmod>2024-06-06T08:52:10+00:00</lastmod>
-  </url>
-  
-  </urlset>`;
+  <!--	created with www.mysitemapgenerator.com	-->
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url>
+<loc>https://www.sses.pk/</loc>
+<lastmod>2024-06-06T12:05:35+01:00</lastmod>
+<priority>0.4</priority>
+</url>
+<url>
+<loc>https://www.sses.pk/services-details/solar-water-pumping-system/</loc>
+<lastmod>2024-06-06T12:05:35+01:00</lastmod>
+<priority>0.1</priority>
+</url>
+<url>
+<loc>https://www.sses.pk/about/</loc>
+<lastmod>2024-06-06T12:05:37+01:00</lastmod>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://www.sses.pk/services/</loc>
+<lastmod>2024-06-06T12:05:38+01:00</lastmod>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://www.sses.pk/project/</loc>
+<lastmod>2024-06-06T12:05:38+01:00</lastmod>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://www.sses.pk/faq/</loc>
+<lastmod>2024-06-06T12:05:39+01:00</lastmod>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://www.sses.pk/contact/</loc>
+<lastmod>2024-06-06T12:05:39+01:00</lastmod>
+<priority>0.8</priority>
+</url>
+</urlset>`;
 }
 
 export async function getServerSideProps({ res }) {
